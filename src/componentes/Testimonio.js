7d7d1ffa44e3bd9311663be1c96267bfae049096
geniusss16 <alfredo.60.faria@gmail.com>
2022-07-04 18:00:54 -0400

    medio dia commit

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,33 +1,21 @@
-import React from 'react';
+import React from "react";
+import "../hojas-de-estilo/Testimonio.css";
 
-
-
-
-
- function Testimonio() {
-
+function Testimonio() {
   return (
-   
-    <div className='contenedor-testimonio'>
-    
-    <img className='imagen-testimonio' 
-      src={require('../imagenes/imagen1.PNG')} 
-      alt='foto de carlos matos'/>
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require("../imagenes/imagen1.png")}
+        alt="foto de carlos matos"/>
 
-      <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Carlos Matos</p>
-        <p className='cargo-testimonio'>Ingeniero en  dibujar Pollas</p>
-        <p className='texto-testimonio'>"Dibujo pollas en la tarde y tiempos libres"</p>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">Carlos Matos</p>
+        <p className="cargo-testimonio">Ingeniero en dibujar Pollas</p>
+        <p className="texto-testimonio">"Dibujo pollas en la tarde y tiempos libres"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
       </div>
-    
-
     </div>
-
-
   );
-   
-
-
 }
 
-export default Testimonio
+export default Testimonio;
